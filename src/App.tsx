f0eed4500cbe20b143cc6b52778606fce6c3d127
kilg <kilg@ya.ru>
2021-06-14 14:36:02 +0300

    create todo list on ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Navbar } from './components/navbar'
+import 'materialize-css/dist/css/materialize.min.css'
+import { ToDoForm } from './components/todo-form';
+import { TodoList } from './components/todo-list';
+import { ITodo } from './interfaces';
 
-function App() {
+
+
+
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
+    setTodos(saved)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+
+
+  const addHandler = (title: string) => {
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false
+    }
+    // setTodos([newTodo, ...todos])
+    setTodos(prev => [newTodo, ...prev]);
+  }
+
+  const toggleHandler = (id: number) => {
+    console.log("asfdasdf")
+    setTodos(prev =>
+      prev.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      }))
+  }
+
+
+  const removeHandler = (id: number) => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className='container'>
+        <ToDoForm onAdd={addHandler} />
+        <TodoList todos={todos} onToggle={toggleHandler} onRemove={removeHandler} />
+      </div>
+    </>
   );
 }
 
